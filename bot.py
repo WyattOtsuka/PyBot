21c5a792182cc4a227522f4ad59c15183c1cf269
marquisss <32654396+marquisss@users.noreply.github.com>
2022-08-28 19:07:07 -0700

    adding first message handler and basic player method headers

--- bot.py
+++ bot.py
@@ -1,9 +1,21 @@
 import discord
+import json
+import sqlite3
+import db_helper as db
+
+# Setting up Login
+f = open("./secrets/config.json")
+configs = json.load(f)
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
+prefix = configs['prefix']
+
+# Setting up the database
+connection = sqlite3.connect("./secrets/PyBot.db")
+cursor = connection.cursor()
 
 @client.event
 async def on_ready():
@@ -14,7 +26,64 @@ async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith(prefix):
+        if not db.has_user(message.author.id):
+            db.new_user(message.author.id)
+            # TODO - Starting prompt
+        else:
+            if message.content == f'{prefix}adv' or message.content == f'{prefix}adventure':
+                adventure(message.author.id)
 
-client.run('your token here')
+            await message.channel.send('Hello!')
+
+
+'''
+PLAYER METHODS
+'''
+@staticmethod
+def adventure(id):
+    '''
+    Rolls dmg dealt and dmg taken. Handles winning and losing
+    '''
+    pass
+
+@staticmethod
+def heal(item, count):
+    '''
+    Uses count amount of item to heal
+    '''
+    pass
+
+@staticmethod
+def points(perk, points):
+    '''
+    Spend points from leveling up to increase perk
+    '''
+    pass
+
+@staticmethod
+def perks(number, tier):
+    '''
+    Gives the user perk number of perk tier
+    '''
+    pass
+
+@staticmethod
+def roll_new_fight():
+    '''
+    Create a new monster with stats based on the players level
+    '''
+    pass
+'''
+PET METHODS
+'''
+@staticmethod
+def xp_share(percent):
+    pass
+
+@staticmethod
+def set_name(name):
+    pass
+
+
+client.run(configs['token'])
