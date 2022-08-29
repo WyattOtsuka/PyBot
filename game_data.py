21094844487e729ca403a3c6f533df5ed27c7780
marquisss <32654396+marquisss@users.noreply.github.com>
2022-08-28 17:50:42 -0700

    adding data structure for player and todo for pet

---
+++ game_data.py
@@ -0,0 +1,23 @@
+class game_user:
+    # id, lvl, xp, hp, atk, def, pet_id, crit_chance, crit_dmg
+    def __init__(self, args):
+        self.id = args[0]
+        self.xp = args[1]
+        self.hp = args[2]
+        self.atk = args[3]
+        self.defence = args[4]
+        self.pet_id = args[5]
+        self.crit_chance = args[6]
+        self.crit_dmg = args[7]
+
+
+class pet:
+    def __init__(self, args):
+        '''
+        self.id =
+        self.xp = 
+        self.hp = 
+        self.atk = 
+        self.defence = 
+        '''
+        pass
